a86f142992b390148fed62531a318fd44d9f3dcd
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-07-14 11:28:03 -0600

    ingreso de productos

--- actividad6.py
+++ actividad6.py
@@ -1,9 +1,9 @@
 producto = {}
 catidad = int(input("Ingrese la cantidad de productos que desea agregar"))
 for i in range(catidad):
-    codigo = input("Ingrese el codigo del producto")
-    nombre_producto = input("Ingrese el nombre del producto")
-    categoria = input("Ingrese la categoria del producto")
-    talla = input("Ingrese la talla del producto")
-    precio = int(input("Ingrese el precio del producto"))
-    cantidad_productos = input("Ingrese la cantidad del producto")
+    codigo = input("Ingrese el codigo del producto: ")
+    nombre_producto = input("Ingrese el nombre del producto: ")
+    categoria = input("Ingrese la categoria del producto: ")
+    talla = input("Ingrese la talla del producto: ")
+    precio = int(input("Ingrese el precio del producto: "))
+    cantidad_productos = input("Ingrese la cantidad del producto: ")
